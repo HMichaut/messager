a6d5d55c335e3616c7560f7c366f3844b637df2a
HMichaut <hectormichaut@gmail.com>
2021-09-02 23:17:20 +0200

    app finalized

---
+++ app/channels/message_channel.rb
@@ -0,0 +1,10 @@
+class MessageChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'message'
+  end
+
+  def receive(data)
+    data['user'] = current_user
+    ActionCable.server.broadcast('message', data)
+  end
+end
